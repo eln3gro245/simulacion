95495822078c922f99e52ab70d8ee2ed920c5034
Jose Hernandez <josehdezbracho@gmail.com>
2026-05-31 23:25:29 -0400

    coneccion de python a godot y funcion de envio de ruta(sin probar)

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
+import websockets
+import asyncio
 import simpy
+import json
 import mapa_grafos as mg
 
 class DeliveryMoto:
-    def __init__(self, env, id_moto, mapa, origen):
+    def __init__(self, env, id_moto, mapa):
         self.env = env
         self.id_moto = id_moto
         self.mapa = mapa
-        self.nodo_actual = origen
+        self.nodo_actual = "centro"
         self.velocidad = 40 # km/h
 
         #inicio mi clase automaticamente cuando llamo a la clase para la simulacion
         self.proceso = env.process(self.run())
         #creamos una lista que almanace la ruta ya definida
-        self.historial = [origen]
+        self.historial = [self.nodo_actual]
 
     def run(self, destino):
         #aqui es donde vamos a correr la simulación
@@ -52,8 +55,53 @@ class DeliveryMoto:
 
             yield self.env.timeout(tiempo_del_viaje)
 
+        async def enviar_ruta(self, pedido, destino, websocket):
+            #aqui mandamos la ruta ya calculada por dikjstra para que se puede visualzar en el godot
+            if pedido:
+                nodo_final = destino
+            else:
+                #damos un mensaje de alerta para ser visualizado en godot 
+                mensaje_alerta = {"Alerta": "Error", "Mensaje": "Inserte una Ruta para Visualizar la Ruta"}
+                await websocket.send(json.dumps(mensaje_alerta))
+            
+            mensaje_ruta = {
+                "Evento": "Moto_ruta",
+                "Ruta": self.historial
+            }
 
-                    
+            await websocket.send(json.dumps(mensaje_ruta))
+            
+
+#esta funcion es la que se encagara de la conexion entre el python y el godot
+async def manejo_server_godot(websocket, mapa, origen):
+    try:
+        #aqui esperamos la primera peticion para activar la simulacion
+        async for mensaje in websocket:
+            peticion = json.loads(mensaje)
+
+            #aqui confirmamos el encabezado del json que determina que accion hace la moto
+            if peticion.get("comando") == "Arrancar_Moto":
+                moto = peticion["Moto"]
+                id_moto = moto["Id_Moto"]
+                destino = moto["Destino"]
+
+                #luego aqui con los datos moto proporcionados por el godot ejecutamos la logica del grafo
+                ruta = mapa.calcular_dijkstra(origen, destino)
+
+                #ahora dentro de python realizamos nosotros la peticion desde python
+                mensaje_json = {
+                    "Evento": "Moto_en_Camino",
+                    "Datos": {
+                        "Estado": "Procesando_Ruta",
+                        "Origen": origen,
+                        "Destino": destino
+                    }
+                }
+
+                await websocket.send(json.dumps(mensaje_json))
+    except websockets.exceptions.ConnectionClosedOK:
+        print("conexion terminada")
+
 
 # Entorno de ejecución
 if __name__ == "__main__":
