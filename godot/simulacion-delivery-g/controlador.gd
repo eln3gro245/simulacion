93b2f70f2c5afdd62ebb43e74fba394361799778
Jose Hernandez <josehdezbracho@gmail.com>
2026-06-06 23:15:26 -0400

    nuevo modulo para trabajar sobre el mapa

---
+++ godot/simulacion-delivery-g/controlador.gd
@@ -0,0 +1,16 @@
+#ahora ya no es solo nodo por que aqui vamos a trabajar sobre el mapa
+extends Node2D
+
+#igual que antes creamos una funcion principal de godot que va escuchar lo que tengo que decir otros mudulos 
+func _ready() -> void:
+	#aqui lo que hacemos es escuchar si el modulo de conexion envio datos 
+	ConexionPython.ruta_python(_visualizacion_rutas())
+
+#aqui es donde vamos a manejar los evento que puedan sucerder
+func _visualizacion_rutas(evento: String, datos: Dictionary) -> void:
+	match evento:
+		"Moto_Ruta":
+			var ruta_lista = datos.get("Ruta")
+			
+		"Moto_en_Camino":
+			pass
